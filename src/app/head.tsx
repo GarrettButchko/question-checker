ccd560ddf8437847d2c7fe037fad278a2b567e5e
Garrett Butchko <garrettwm2005@gmail.com>
2026-02-05 12:50:16 -0500

    add head

---
+++ src/app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="google-adsense-account" content="ca-pub-8261962597301587" />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8261962597301587"
+        crossOrigin="anonymous"
+      />
+    </>
+  )
+}
